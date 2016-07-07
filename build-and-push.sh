d647755251661fc52586d2493fd87a87a6b3235c
Joonas Rouhiainen <joonas.rouhiainen@reaktor.com>
2016-07-07 15:13:32 +0300

    Push to ECR instead of Docker Hub.

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 DOCKER_IMAGE="yleisradio/newrelic-cloudwatch"
+ECR_REPOSITORY="352476883983.dkr.ecr.eu-west-1.amazonaws.com/newrelic-cloudwatch-production"
 
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 docker_image_tag" >&2
   exit 1
 fi
 
+eval $(asu infra-aws-yle ops aws ecr get-login --region eu-west-1)
 docker build --no-cache -t "$DOCKER_IMAGE":"$1" .
-docker push "$DOCKER_IMAGE":"$1"
+docker tag "$DOCKER_IMAGE":"$1" "$ECR_REPOSITORY":"$1"
+docker push "$ECR_REPOSITORY":"$1"
